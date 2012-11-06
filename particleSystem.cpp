9961ecf700953c2435057ad97ad8aa6daac970b8
Jonathan Brenner <brennj3@rpi.edu>
2012-11-06 14:04:41 -0500

    Work we did today

--- particleSystem.cpp
+++ particleSystem.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #ifdef __APPLE__
 #  include <GLUT/glut.h>
@@ -10,101 +11,141 @@ using namespace std;
 
 // Globals.
 static float Xangle = 0.0, Yangle = 0.0, Zangle = 0.0; // Angles to rotate scene.
+static float displacement = 0.1;
+static float position = 0.0;
+static unsigned int redrawTime = 1000;
+
+struct particle{
+    float xpos;
+    float ypos;
+    float zpos;
+};
+
+vector <particle> parts;
 
 // Drawing routine.
 void drawScene(void)
 {
-   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   glLoadIdentity();
-       
-   // Rotate scene.
-   glRotatef(Zangle, 0.0, 0.0, 1.0);
-   glRotatef(Yangle, 0.0, 1.0, 0.0);
-   glRotatef(Xangle, 1.0, 0.0, 0.0);
-
+    glClear (GL_COLOR_BUFFER_BIT);// | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+         
+     // Rotate scene.
+    glRotatef(Zangle, 0.0, 0.0, 1.0);
+    glRotatef(Yangle, 0.0, 1.0, 0.0);
+    glRotatef(Xangle, 1.0, 0.0, 0.0);
+    
+    glColor3f(0.0, 0.0, 0.0);
+    
+    //if(displacement + 0.0 > 100.0){
+    //    displacement = 0.1;
+    //}
+    
+    for(unsigned int i = 0; i < parts.size(); i++){
+        glTranslatef(parts[i].xpos, parts[i].ypos, parts[i].zpos);
+        parts[i].ypos += displacement;
+        glutSolidSphere(1.0, 20.0, 20.0);
+    }
+    glFlush();
+    glutPostRedisplay();
 }
 
 // Initialization routine.
 void setup(void) 
 {
+    // set the background/clearing color
+    glClearColor(1.0, 1.0, 1.0, 0.0);
 }
 
 // OpenGL window reshape routine.
 void resize(int w, int h)
 {
-   glViewport(0, 0, (GLsizei)w, (GLsizei)h); 
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   glFrustum(-5.0, 5.0, -5.0, 5.0, 5.0, 100.0);
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h); 
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
 
-   glMatrixMode(GL_MODELVIEW);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
 // Keyboard input processing routine.
 void keyInput(unsigned char key, int x, int y)
 {
-   switch(key) 
-   {
-      case 27:
-         exit(0);
-         break;
-      case 'x':
-         Xangle += 5.0;
+    switch(key) 
+    {
+        case 27:
+            exit(0);
+            break;
+        case 'x':
+            Xangle += 5.0;
 		 if (Xangle > 360.0) Xangle -= 360.0;
-         glutPostRedisplay();
-         break;
-      case 'X':
-         Xangle -= 5.0;
+            glutPostRedisplay();
+            break;
+        case 'X':
+            Xangle -= 5.0;
 		 if (Xangle < 0.0) Xangle += 360.0;
-         glutPostRedisplay();
-         break;
-      case 'y':
-         Yangle += 5.0;
+            glutPostRedisplay();
+            break;
+        case 'y':
+            Yangle += 5.0;
 		 if (Yangle > 360.0) Yangle -= 360.0;
-         glutPostRedisplay();
-         break;
-      case 'Y':
-         Yangle -= 5.0;
+            glutPostRedisplay();
+            break;
+        case 'Y':
+            Yangle -= 5.0;
 		 if (Yangle < 0.0) Yangle += 360.0;
-         glutPostRedisplay();
-         break;
-      case 'z':
-         Zangle += 5.0;
+            glutPostRedisplay();
+            break;
+        case 'z':
+            Zangle += 5.0;
 		 if (Zangle > 360.0) Zangle -= 360.0;
-         glutPostRedisplay();
-         break;
-      case 'Z':
-         Zangle -= 5.0;
+            glutPostRedisplay();
+            break;
+        case 'Z':
+            Zangle -= 5.0;
 		 if (Zangle < 0.0) Zangle += 360.0;
-         glutPostRedisplay();
-         break;
-      default:
-         break;
-   }
+            glutPostRedisplay();
+            break;
+        default:
+            break;
+    }
 }
 
 // Routine to output interaction instructions to the C++ window.
 void printInteraction(void)
 {
-   
+    
+}
+
+// Timer redraw
+void newParticle(int value)
+{
+    particle p;
+    p.xpos = 0;
+    p.ypos = 0;
+    p.zpos = 0;
+    parts.push_back(p);
+    glutPostRedisplay();
+    glutTimerFunc(redrawTime, newParticle, 1);
 }
 
 // Main routine.
 int main(int argc, char **argv) 
 {
-   printInteraction();
-   glutInit(&argc, argv);
-   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); 
-   glutInitWindowSize(500, 500);
-   glutInitWindowPosition(100, 100); 
-   glutCreateWindow("Particle System"); 
-   setup(); 
-   glutDisplayFunc(drawScene); 
-   glutReshapeFunc(resize);  
-   glutKeyboardFunc(keyInput);
-   glutMainLoop(); 
+    printInteraction();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100); 
+    glutCreateWindow("Particle System"); 
+    setup(); 
+    glutDisplayFunc(drawScene); 
+    glutReshapeFunc(resize);  
+    glutKeyboardFunc(keyInput);
+    glutTimerFunc(redrawTime, newParticle, 1);
+    glutMainLoop(); 
 
-   return 0;  
+    return 0;
 }
 
 
